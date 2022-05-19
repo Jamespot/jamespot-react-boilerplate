3554f1102a135cdb85748bbd2492140bc483f319
Romaric Pighetti <romaric.pighetti@jamespot.com>
2022-05-19 09:53:34 +0200

    Corrected the app path in a comment

--- src/extensions/Demo/Demo.const.tsx
+++ src/extensions/Demo/Demo.const.tsx
@@ -2,7 +2,7 @@ import { ReactExtensionProps } from 'jamespot-react-core';
 
 export const DemoAppConst: ReactExtensionProps = {
     extensionName: 'Demo',
-    route: 'boilerplate/demo', // App url will be /ng/r/boilerplate/demo
+    route: 'boilerplate/demo', // App url will be /ng/rr/boilerplate/demo
     idAnchor: 'boilerplate/demo', // The extension will be injected on a div with this id
     reducerName: 'demoReducer', // If we use reduce, we can specify the reducer name
     gabarit: 'app-1-cols',
